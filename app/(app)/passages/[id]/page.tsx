b79c86edc724ec307684a78bafae876cf252fc61
Julius Liu <codebase@juliusliu.dev>
2025-02-20 14:28:40 -0500

    Add soft navigation button

--- app/(app)/passages/[id]/page.tsx
+++ app/(app)/passages/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Toaster } from "@/components/ui/sonner";
 import Passage from "@/components/passage";
+import { NavigationBackArrow } from "@/components/navigation";
 
 async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
@@ -8,6 +9,7 @@ async function Page({ params }: { params: Promise<{ id: string }> }) {
   return (
     <main className="p-5 flex w-full justify-center">
       <div className="w-full max-w-3xl mt-10 pt-5">
+        <NavigationBackArrow className="mb-5" />
         <Passage passageId={id} />
       </div>
       <Toaster richColors />
---
+++ components/navigation.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { ArrowLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { cn } from "@/lib/utils";
+
+export const NavigationBackArrow: React.FC<{ className?: string }> = ({ className }) => {
+  const router = useRouter();
+  return (
+    <ArrowLeft className={cn(className, 'cursor-pointer')} onClick={router.back} />
+  )
+}
