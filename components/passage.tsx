762fa9a767ca55c3e33514fd5c24cc81f2a29158
Julius Liu <codebase@juliusliu.dev>
2025-02-24 22:41:26 -0500

    build: add commitlint

--- components/passage.tsx
+++ components/passage.tsx
@@ -86,7 +86,7 @@ export function PassageTitle({ passage }: PassageTitleProps) {
 
 const Copy = ({ text }: { text: string }) => {
   return (
-    <TooltipProvider delayDuration={700}>
+    <TooltipProvider>
       <Tooltip>
         <TooltipTrigger>
           <ClipboardCopy
