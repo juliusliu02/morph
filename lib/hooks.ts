3029ee085e006c73bff665c99dd1014552fc64ce
Julius Liu <codebase@juliusliu.dev>
2025-03-04 17:16:48 -0500

    refactor: use client-only for hooks

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,11 +1,10 @@
+import "client-only";
 import { useEffect, useState } from "react";
 
 export function useMediaQuery(query: string) {
   const [matches, setMatches] = useState(false);
 
   useEffect(() => {
-    if (typeof window === "undefined") return;
-
     const media = window.matchMedia(query);
     if (media.matches !== matches) {
       setMatches(media.matches);
