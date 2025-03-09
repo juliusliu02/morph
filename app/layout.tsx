5292160f4da98eb18461ebdde8fc8e721706e9b1
Julius Liu <codebase@juliusliu.dev>
2025-03-09 12:26:30 -0400

    fix: reposition landing page colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,12 @@ export default function RootLayout({
         className={`${sourceSans3.className} antialiased bg-slate-50 dark:bg-slate-950 relative`}
       >
         <div
-          className="absolute top-[12rem] -z-10 h-[32rem] rounded-full bg-[#DFE7F6] blur-[10rem]
-                   right-[calc(50svw-16rem)] w-[20rem] dark:bg-[#10497E]"
+          className="absolute top-[10rem] -z-10 h-[28rem] rounded-full bg-[#DFE7F6] blur-[15rem]
+                   right-[calc(46svw-20rem)] w-[20rem] sm:w-[26rem] dark:bg-[#10497E]"
         />
         <div
-          className="absolute top-[5rem] -z-10 h-[24rem] w-[20rem] rounded-full bg-[#DDE5DC] blur-[10em]
-                    left-[calc(50svw-20rem)] sm:h-[28rem] dark:bg-[#46AFA1]"
+          className="absolute top-[6rem] -z-10 h-[24rem] w-[20rem] rounded-full bg-[#DDE5DC] blur-[15em]
+                    left-[calc(48svw-18rem)] sm:w-[25rem] dark:bg-[#46AFA1]"
         />
         {children}
         <SpeedInsights />
