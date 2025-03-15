de2fd1d726416c2b891720672b9dab39f17ddf35
Julius Liu <codebase@juliusliu.dev>
2025-03-14 22:05:07 -0400

    feat: update prompts to improve feedback content

--- lib/llm/prompts.ts
+++ lib/llm/prompts.ts
@@ -3,18 +3,22 @@ export const systemPrompt = `You are a helpful essay editor with a sharp taste.
 Your job is to modify essays from users who are not native speakers of English.
 Aim to polish the passage without losing its originality.
 
-For every edit you make, examine the original text and provide a brief feedback with CEFR gradings for both the original and the edit. Make minimal modifications and explain how the edit is better.`;
+For every edit you make, examine the original text and provide a brief feedback.
+Make minimal modifications and explain how the edit is better.`;
 
 export const grammarEditPrompt = `For the following content, complete the following tasks based on standard Canadian English usage:
 - Correct only the grammatical mistakes in this text.
 - Use correct and consistent spellings and punctuations.
-- Consolidate related adjacent paragraphs into one.`;
+- Consolidate related adjacent paragraphs into one.
+- Show the edits in categories and provide a general and comprehensive feedback.`;
 
 export const lexicalEditPrompt = `For the following content, paraphrase or rewrite the content that reads repetitive or unnatural.
 - Employ precise vocabulary and avoid obscure and uncommon words.
 - Construct clear, concise, and straightforward sentences with accurate word choice.
-- Maintain a sentence-to-sentence correspondence between the source and the edit.`;
+- Maintain a sentence-to-sentence correspondence between the source and the edit.
+- In the feedback, evaluate and contrast the writing styles for both texts.`;
 
-export const logicalEditPrompt = `For the following content, analyze the examples and arguments presented in the article, evaluate their relevance and accuracy, and finish the tasks below.
+export const logicalEditPrompt = `For the following content, analyze the examples and arguments presented in the article, evaluate their relevance and accuracy, and finish the tasks below:
 - Make changes to the article's structure to improve logical coherence of the passage.
-- Replace unnecessary, inaccurate, or irrelevant arguments with clear, concrete, and specific explanations or examples where applicable and explain your edits.`;
+- Replace unnecessary, inaccurate, or irrelevant arguments with clear, concrete, and specific explanations or examples where applicable and explain your edits.
+- Give feedback for each paragraph and explain how improvements are made.`;
